9e7a58e57d10a21957e918b8b79ad5c08768746e
gmt <generalgmt@yahoo.com>
2014-07-13 21:17:02 +0100

    weatherapi

---
+++ js/map.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+	var city_sel ;
+	// var temperature = 45;
+	$("#form").submit(function(event){
+	event.preventDefault();
+	city_sel = $("#city_name").val();
+	if(/^[a-zA-Z-, ]*$/.test(city_sel) == false)
+	{
+		form.reset();
+    	alert('Your search string contains illegal characters.');
+	}
+
+	else
+	{	form.reset();
+		$("#exp").hide();
+		$("#cetee, #map, #temp, #table_div, #weat").fadeIn("slow");
+		// $("#overlay").html(temperature + " F");
+		// alert("Welcome");
+		// console.log(city_sel);
+		var fourcast_url = "https://api.foursquare.com/v2/venues/search?near=" + city_sel + "&client_id=NK2F2DN4LL5ZRKHX3QLIUULNCET5OHVX1RYKMDQQ4XY4RFFH&client_secret=00F0HEOA2VAP5XS2OIGKGOTQPM522J1G4M341BY5DQO21CTL&v=20140710&callback=?";
+		function display_fourcast(fourecast_response){
+			console.log(fourecast_response.meta.code);
+			if(fourecast_response.meta.code != 200)
+			{
+				$("#display_selected").html("City not found");
+			}
+			else
+			{
+				var result1 = fourecast_response.response.geocode.feature;
+				var result = result1.geometry.center;
+				var city = result1.displayName;
+				var lat = result.lat;
+				var lng = result.lng;
+				console.log(city);
+				console.log(lat);
+				console.log(lng);
+				$("#display_selected").html("You have selected" + " " + city + 
+					"." + " " + "Coordinates are.." + " " + lat +"," + lng);
+				var forcast_url = "https://api.forecast.io/forecast/761080e4acfe83e078084db199ffe7c3/" + lat + "," + lng + "?callback=?";
+				// var me = [2000, 3.3];
+				// var me1 = [2002, 6];
+				var me2 = "<table ><tr><th>Time</th><th>Wind Speed</th><th>Wind Bearing</th></tr>";
+				function display_forcast(forecast_response){
+					var result_curr = forecast_response.currently.temperature;
+					var daily_result = forecast_response.daily.data;
+					// var normal_time = result.time;
+					var graph_data = [];
+					var options = {
+				        lines: { show: true },
+				        points: { show: true },
+				        xaxis: { tickDecimals: 0, tickSize: 1 }
+			    	};
+					console.log(result_curr);
+					console.log(daily_result);
+					$("#overlay").html(result_curr);
+					$.each(daily_result, function(i, item){
+						var date = new Date(item.time * 1000);
+						me2 += "<tr><td>" + date + "</td>" +
+						"<td>" + item.windSpeed + "</td>" +
+						"<td>" + item.windBearing + 
+						"</td></tr>";
+				        graph_data.push([i,item.windSpeed]);
+					});
+					me2 += "</table>";
+					$("#tab").html(me2);
+					$.plot($(".container"), [graph_data], options);
+				}
+				$.getJSON(forcast_url, display_forcast);
+				// var data = [[1999,4], [2003, 1.3], me, [2001, 2.0], me1, [2004, 2.5], [2005, 2.0], [2006, 3.1], [2007, 2.9], [2008, 0.9]];
+				// $.plot($(".container"), [ data ], options);
+  	
+			}
+		}
+			$.getJSON(fourcast_url, display_fourcast);
+	}
+
+	});
+});	
